5472248149e21f13855d0931e40333ff691f5d86
Osman B. Koc <osmanberkekoc@hotmail.com>
2025-10-13 21:50:55 +0300

    update .gitignore

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import whisper
 import os
 
 def transcribe_audio():
-    pass
+    model = whisper.load_model("turbo")
+    result = model.transcribe("audio.mp3")
+    return result["text"]
 
 def create_subtitles():
     pass
